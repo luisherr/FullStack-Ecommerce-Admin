26caa1262622a47fdacb9c8a032cda278325f684
Luis Herrera <luis378962@gmail.com>
2023-04-24 23:44:11 -0600

    final admin authentication

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
+import { useSession, signIn, signOut } from "next-auth/react";
+
+
 export default function Home() {
-  return (
-    <div className="bg-blue-900 w-screen h-screen flex items-center">
-      <div className="text-center w-full">
-      <button className="bg-white p-2 px-4 rounded-lg">Iniciar sesión en Google</button>
+  const { data: session } = useSession();
+  if (!session) {
+    return (
+      <div className="bg-blue-900 w-screen h-screen flex items-center">
+        <div className="text-center w-full">
+          <button onClick={() => signIn('google')} className="bg-white p-2 px-4 rounded-lg">Iniciar sesión en Google</button>
+        </div>
       </div>
-    </div>
-  )
+    );
+  }
+
+  return (
+    <div>Ya te haz logeado {session.user.email}</div>
+  );
 }
